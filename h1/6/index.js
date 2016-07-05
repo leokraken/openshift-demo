b49f739b129485e867c1deb4895081c5936ec35c
leokraken <joleocl@gmail.com>
2016-07-05 20:42:07 -0300

    H1C6 demo

---
+++ h1/6/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.get('/error', function (req, res) {
+  process.exit(0);
+  res.send(a);
+});
+
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
+});
