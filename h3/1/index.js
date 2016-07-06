f47a7fe30f58e90f16b915265b5dc2517ec2205c
leokraken <joleocl@gmail.com>
2016-07-06 01:07:32 -0300

    Fix protocol

--- h3/1/index.js
+++ h3/1/index.js
@@ -3,7 +3,7 @@ var app = express();
 var request = require('request');
 
 //service
-var host = process.env.V2_PORT_8080_TCP_ADDR + ':' + process.env.V2_SERVICE_PORT_8080_TCP;
+var host = 'http://' + process.env.V2_PORT_8080_TCP_ADDR + ':' + process.env.V2_SERVICE_PORT_8080_TCP;
 
 
 app.get('/interact', function (req, res) {
