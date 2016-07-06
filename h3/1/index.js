616d0f2938e8665c7cbc5913e24ef297c3512f78
leokraken <joleocl@gmail.com>
2016-07-06 00:54:01 -0300

    Docker version not updated, adding env parameters

--- h3/1/index.js
+++ h3/1/index.js
@@ -3,7 +3,7 @@ var app = express();
 var request = require('request');
 
 //service
-var host = 'v2';
+var host = process.env.V2_PORT_8080_TCP_ADDR + ':' + process.env.V2_SERVICE_PORT_8080_TCP;
 
 
 app.get('/interact', function (req, res) {
